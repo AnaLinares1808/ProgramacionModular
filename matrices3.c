a55d328d4b5e69e791aa7339102dd74fa85b6890
Uriel Solalíndez <uriel22_18@ciencias.unam.mx>
2022-11-24 17:10:06 -0600

    Se modificó "matrices3.c"

--- matrices3.c
+++ matrices3.c
@@ -6,22 +6,22 @@
 #define MAX 10
 int main(int argc, char** args)
 {
-	int matriz[MAX][MAX], col, reng, i=0, j=0;
+	int matriz[MAX][MAX], col, fila, i=0, j=0;
+	printf("Escribe cuantas filas quieres: ");
+	scanf("%d", &fila);
 	printf("Escribe cuantas columnas quieres: ");
 	scanf("%d", &col);
-	printf("Escribe cuantos renglones quieres: ");
-	scanf("%d", &reng);
-	for(i = 0;i < reng;i++) //Aqui se leen los datos
+	for(i = 0;i < fila;i++)
 	{
 		for(j = 0;j < col;j++) {
-			printf("Escribe los valores [%d][%d]==>\t", i+1, j+1);
+			printf("Escribe los valores de la posicion [%d][%d]==>\t", i+1, j+1);
 			scanf("%d", &matriz[i][j]);
 		}
 	}
 
 	printf("\n\n\tMatriz original"); //Imprime los datos de la matriz declarada
 	printf("\n");
-	for(i = 0;i < reng;i++){
+	for(i = 0;i < fila;i++){
 		printf("\n");
 	for(j = 0;j < col;j++){
 		printf(" %6d ", matriz[i][j]);
@@ -31,7 +31,7 @@ int main(int argc, char** args)
 	printf("\n");
 	for(i = 0;i < col;i++){
 		printf("\n");
-	for(j = 0;j < reng;j++){
+	for(j = 0;j < fila;j++){
 		printf(" %6d ", matriz[j][i]);
 		} }
 
